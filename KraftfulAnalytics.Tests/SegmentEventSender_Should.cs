fdc552f0c75ec3af16a7f44db479d6c4443a6f26
Jacob Gable <jacob.gable@gmail.com>
2022-10-05 12:23:49 -0700

    Initial library and tests
    - New solution with core library and tests projects
    - Using xUnit for testing framework and Moq for mocks
    - Basic tests
    - Basic README docs and MIT LICENSE

---
+++ KraftfulAnalytics.Tests/SegmentEventSender_Should.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using KraftfulAnalytics.Core;
+using Moq;
+using Segment;
+using Xunit;
+
+namespace KraftfulAnalytics.Tests
+{
+    public class SegmentEventSender_Should
+    {
+        private Mock<IAnalyticsClient> mockClient;
+
+        public SegmentEventSender_Should()
+        {
+            mockClient = new Mock<IAnalyticsClient>();
+        }
+
+        [Fact]
+        public void IdentifyAndKeepUserId()
+        {
+            var sender = new SegmentEventSender(mockClient.Object);
+
+            var newUserId = "test-user-id-3";
+            sender.Identify(newUserId);
+            mockClient.Verify(client => client.Identify(newUserId, null), Times.Once(), "Should call Identify with new user id");
+
+            sender.Track("Test Feature A");
+            mockClient.Verify(client => client.Track(newUserId, "Test Feature A", null as IDictionary<string, object>), Times.Once(), "Should call Track with new user id");
+            mockClient.VerifyNoOtherCalls();
+        }
+    }
+}
+
---
+++ KraftfulAnalytics/Core/SegmentEventSender.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Segment;
+
+namespace KraftfulAnalytics.Core
+{
+    public class SegmentEventSender : IEventSender
+    {
+        protected static string KRAFTFUL_INGESTION_URL = "https://analytics-ingestion.kraftful.com";
+        protected IAnalyticsClient client;
+
+        protected string lastUserId;
+
+        public SegmentEventSender(string apiKey)
+            : this(
+                  new Client(apiKey, new Config(
+                    host: KRAFTFUL_INGESTION_URL
+                  ))
+              )
+        {
+
+        }
+
+        public SegmentEventSender(IAnalyticsClient client)
+        {
+            this.client = client;
+        }
+
+        public void Identify(string userId)
+        {
+            Identify(userId, null);
+        }
+
+        public void Identify(string userId, IDictionary<string, object> properties)
+        {
+            client.Identify(userId, properties);
+            lastUserId = userId;
+        }
+
+        public void Track(string name)
+        {
+            Track(name, null);
+        }
+
+        public void Track(string name, IDictionary<string, object> properties)
+        {
+            client.Track(lastUserId, name, properties);
+        }
+    }
+}
+

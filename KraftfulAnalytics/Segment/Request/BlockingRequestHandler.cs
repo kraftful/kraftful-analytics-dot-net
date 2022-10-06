ee80309e61bf7a5e73717dad18d4e413aa39346a
Jacob Gable <jacob.gable@gmail.com>
2022-10-06 12:57:11 -0700

    Fixup sentAt timestamp format to be UTC

--- KraftfulAnalytics/Segment/Request/BlockingRequestHandler.cs
+++ KraftfulAnalytics/Segment/Request/BlockingRequestHandler.cs
@@ -158,7 +158,7 @@ namespace Segment.Request
                 Uri uri = new Uri(_client.Config.Host + "/v1/import");
 
                 // set the current request time
-                batch.SentAt = DateTime.Now.ToString("o");
+                batch.SentAt = DateTime.Now.ToUniversalTime().ToString("o");
 
                 string json = JsonConvert.SerializeObject(batch);
 
